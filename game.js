7e16e2a969987106320e8af1febabd79e8a8d0bb
Oval-Maria127 <ovalmaria1@gmail.com>
2021-09-02 20:53:18 +0000

    delete usless file

---
+++ game.js
@@ -0,0 +1,25 @@
+const question = document.querySelector('#question')
+const choices = Array.from(document.querySelectorAll('choice-Text'))
+/*const progressText = document.querySelector('#progressText')*/
+const scoreText = document.querySelector('#score')
+/*const progressBarFull = document.querySelector('#progressBarFull')*/
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let avalible questions = []
+
+let questions = [
+    {
+    question:'  What is 2 + 8?';
+    choice1:'7';
+    choice2:'10';
+    choice3:'21';
+    answer:2;
+    }
+]
+/*Repeat the above for every question */
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
