6f7da6eaa8964eea95069fb0b8e47f97ee01e093
Oval-Maria127 <ovalmaria1@gmail.com>
2021-09-06 15:01:32 +0000

    create end.html

--- game.js
+++ game.js
@@ -1,23 +1,23 @@
 
 const question = document.querySelector('#question')
 const choices = Array.from(document.querySelectorAll('choice-Text'))
-/*const progressText = document.querySelector('#progressText')*/
+const progressText = document.querySelector('#progressText')
 const scoreText = document.querySelector('#score')
-/*const progressBarFull = document.querySelector('#progressBarFull')*/
+const progressBarFull = document.querySelector('#progressBarFull')
 
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
-let avalible questions = []
+let avalibleQuestions = []
 
 let questions = [
     {
-    question:'  What is 2 + 8?';
-    choice1:'7';
-    choice2:'10';
-    choice3:'21';
-    answer:2;
+    question:'  What is 2 + 8?',
+    choice1:'7',
+    choice2:'10',
+    choice3:'21',
+    answer:2
     }
 ]
 /*Repeat the above for every question */
@@ -39,12 +39,12 @@ return window.location.assign('/end.html')
 }
 
 questionCounter ++
-progressText.innerText = Question ${questionCounter} of ${MAX_QUESTIONS}
-progressBarFull.style.width = ${(questionCounter / MAX_QUESTIONS)* 100}%
-const questionsIndex = math.floor(math.random()* avalibleQuestions.length)currentQuestion = avalibleQuestions[questionsIndex]
+progressText.innerText = Question; $;{questionCounter} of ;{MAX_QUESTIONS}
+progressBarFull.style.width = $;{(questionCounter / MAX_QUESTIONS)* 100}%
+const questionsIndex = math.floor(math.random()* avalibleQuestions.length),currentQuestion = avalibleQuestions[questionsIndex]
 
 question.indexText = currentQuestion.question
-choices.forEach/choice => {
+choices.forEach/choice; => {
     const number = choice.dataset['number']
     choice.innerText = currentQuestion['choice + number']
 })
@@ -53,9 +53,9 @@ avalibleQuestions.splice(questionsIndex, 1)
 acceptingAnswers = true
 }
 
-choices.forEach/choice => {
-    choice.addEventListener('click' , e =>{
-        if(!acceptingAnswers = false) return
+choices.forEach/choice; => {
+    choice.addEventListener('click' , e => }
+        if(!acceptingAnswers) = false)return
 
         acceptingAnswers = false
         const selectedChoice = e.target
